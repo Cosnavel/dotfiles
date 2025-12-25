42b8a73bd3d5bc9490abcc177bf92804f83ff94b
Cosnavel <42392570+Cosnavel@users.noreply.github.com>
2025-12-25 12:49:51 +0100

    add power.sh script to open YouTube link

---
+++ raycast/power.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Power
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤮
+
+open "https://www.youtube.com/watch?v=lKjw3oWrMrI"
